72869e2624c939ea2df2bc36def90772914a363d
Fanny Holmström <fannyholmstrom@icloud.com>
2024-02-05 15:26:17 +0100

    feat: install MongoDB & name database

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const mongoClient = require('mongodb').MongoClient;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const { log } = require('console');
 
 const app = express();
 
@@ -15,6 +18,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoClient.connect('mongodb://127.0.0.1:27017')
+.then(client => {
+    console.log("MongoDB");
+
+    const db = client.db("fanny-holmstrom");
+    app.locals.db = db;
+})
 
 app.use('/', indexRouter);
 
