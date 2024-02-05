c7fc5be6bba1ff73788b898a3e7419309cfb0852
Fanny Holmström <fannyholmstrom@icloud.com>
2024-02-05 16:36:31 +0100

    feat: add /api/users/:userID endpoint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const csv = require('csv-parser');
 
+const mockUsers = [];
+
+// read users.csv data
+fs.createReadStream('./mockdata/users.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    mockUsers.push(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed.');
+  });
+
+function formatUser(user) {
+  const { id, first_name, last_name, email } = user;
+  return {
+    id, 
+    name: `${first_name} ${last_name}`,
+    email,
+  };
+}
 
 // GET /api/users (get all users)
 router.get('/', (req, res) => {
-  // logic for fetching users
-  res.json({ message: 'Get all users' });
+  try {
+    // format users without password
+    const formattedUser = mockUsers.map(formatUser);
+
+    // respond with formatted users
+    res.json({ users: formattedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// GET /api/users/userID (get specific user)
+router.get('/:userID', (req, res) => {
+  try {
+    const userId = req.params.userID;
+    const user = mockUsers.find((u) => u.id === userId);
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      // format user
+      const formattedUser = formatUser(user);
+
+      res.json({ user: formattedUser });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
 
